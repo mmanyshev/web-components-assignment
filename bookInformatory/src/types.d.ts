f5b27d730e158e62b62d643d4c29d22c01c98599
Michael Manyshev <mikhail.manyshev@firstlinesoftware.com>
2019-07-09 15:13:55 +0300

    BookInformatory: properly linted

--- bookInformatory/src/types.d.ts
+++ bookInformatory/src/types.d.ts
@@ -1,11 +1,11 @@
 
 declare module "*.css" {
-  var _: string;
+  const _: string;
   export default _;
 }
 
 declare module "*.html" {
-  var _: string;
+  const _: string;
   export default _;
 }
 
@@ -13,18 +13,4 @@ declare namespace Intl {
   const RelativeTimeFormat: any;
 }
 
-declare class webkitSpeechRecognition extends SpeechRecognition {
-
-  continuous: boolean;
-  interimResults: boolean;
-
-  abort: () => void;
-  start: () => void;
-  stop: () => void;
-
-  onstart: () => void;
-  onresult: (event: any) => void;
-  onerror: (event: any) => void;
-  // onend: (event: any) => void;
-
-}
+declare class webkitSpeechRecognition extends SpeechRecognition {}
